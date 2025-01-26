a95ecd632e757669eeaf6480883b638ca3f3f81c
somewhat-armoured-shrimp <tor24017@byui.edu>
2025-01-25 17:01:14 -0800

    ehh discover but unoptimised img because reasons

---
+++ chamber/scripts/chamber.js
@@ -0,0 +1,27 @@
+document.querySelector("#lastModified").textContent = document.lastModified;
+
+const hamburgr = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamburgr.addEventListener('click', () => {
+    hamburgr.classList.toggle('open');
+    navigation.classList.toggle('open');
+});
+
+// dude i need to take a class for just javascript. not because
+// i like it but because worrying about html and css at the same time is
+// just not working out, man.
+
+const visits = document.querySelector('#visits');
+
+let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
+
+if (numVisits == 0) {
+    visits.textContent = `Welcome! Let us know if you have any questions.`;
+} else {
+    visits.textContent = `Why are you here more than once. I'm flattered but why`
+}
+
+numVisits++;
+
+localStorage.setItem("numVisits-ls", numVisits);
